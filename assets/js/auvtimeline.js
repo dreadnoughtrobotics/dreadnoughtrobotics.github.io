b3628a770863be4ecc64195e3881f904106f0baf
kritishpalrecha <kritishgalaxy10@gmail.com>
2021-09-21 17:43:47 +0530

    timeline issues fixed and content added

--- assets/js/auvtimeline.js
+++ assets/js/auvtimeline.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 $(window).on('scroll',function () {
-    var top=$(window).scrollTop()/2.5;
+    var top=$(window).scrollTop()/2;
  
   $('.timeline-icon').css({'transform':'translate(-50%,-50%) rotate('+top+'deg)'});
 })
